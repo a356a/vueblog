503912d400737b2e2fe9c445849af336fbf124cb
a356a <124365389+a356a@users.noreply.github.com>
2023-10-28 09:29:25 +0800

    将环境地址改为固定服务

--- vueblog-java/src/main/resources/vueblog.sql
+++ vueblog-java/src/main/resources/vueblog.sql
@@ -60,4 +60,21 @@ CREATE TABLE `m_user` (
 -- ----------------------------
 -- Records of m_user
 -- ----------------------------
-INSERT INTO `m_user` VALUES ('1', 'markerhub', 'https://image-1300566513.cos.ap-guangzhou.myqcloud.com/upload/images/5a9f48118166308daba8b6da7e466aab.jpg', null, '96e79218965eb72c92a549dd5a330112', '0', '2020-04-20 10:44:01', null);
+INSERT INTO `m_user` VALUES ('1',
+                             'markerhub',
+                             'https://image-1300566513.cos.ap-guangzhou.myqcloud.com/upload/images/5a9f48118166308daba8b6da7e466aab.jpg',
+                             null,
+                             '96e79218965eb72c92a549dd5a330112',
+                             '0',
+                             '2020-04-20 10:44:01',
+                             null);
+
+INSERT INTO `m_user` VALUES ('2',
+                             'dengliang',
+                             'https://image-1300566513.cos.ap-guangzhou.myqcloud.com/upload/images/5a9f48118166308daba8b6da7e466aab.jpg',
+                             null,
+                             '8ddcff3a80f4189ca1c9d4d902c3c909',
+                             '0',
+                             '2020-04-20 10:44:01',
+                             null);
+
